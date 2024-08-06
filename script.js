5c02f33aaa9623cf3e694db0b11a3e939790317b
k14s3 <158744755+k14s3@users.noreply.github.com>
2024-08-06 08:59:53 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function showPage(pageId) {
+    const pages = document.querySelectorAll('.container');
+    pages.forEach(page => {
+        page.style.display = 'none';
+    });
+    document.getElementById(pageId).style.display = 'block';
+}
