587ee4964f263a599928f11a9bf4cbd17e070a58
k14s3 <158744755+k14s3@users.noreply.github.com>
2024-08-06 09:23:48 +0300

    Updatednew script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function showPage(pageId) {
     const pages = document.querySelectorAll('.container');
-    pages.forEach(page => page.style.display = 'none');
+    pages.forEach(page => {
+        page.style.display = 'none';
+    });
     document.getElementById(pageId).style.display = 'block';
 }
