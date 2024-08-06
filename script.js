5a3ab786fd1df4ce9b3219825dee8ccc844a2407
k14s3 <158744755+k14s3@users.noreply.github.com>
2024-08-06 09:18:10 +0300

    Updfdgate script.js

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 function showPage(pageId) {
     const pages = document.querySelectorAll('.container');
-    pages.forEach(page => {
-        page.style.display = 'none';
-    });
+    pages.forEach(page => page.style.display = 'none');
     document.getElementById(pageId).style.display = 'block';
 }
